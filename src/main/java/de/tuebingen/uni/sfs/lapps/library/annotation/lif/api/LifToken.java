457c6bf1c5cb685c3f4912167fb4c7bbc804022b
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2018-01-16 15:12:17 +0100

    moved lif related api from converter to Lif library

---
+++ src/main/java/de/tuebingen/uni/sfs/lapps/library/annotation/lif/api/LifToken.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tuebingen.uni.sfs.lapps.library.annotation.lif.api;
+
+import de.tuebingen.uni.sfs.lapps.library.annotation.xb.AnnotationInterpreter;
+import java.util.List;
+
+/**
+ *
+ * @author Mohammad Fazleh Elahi
+ */
+public interface LifToken {
+    public List<AnnotationInterpreter> getTokenList(); 
+}
